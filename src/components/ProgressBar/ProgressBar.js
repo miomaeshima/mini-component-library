19dcb9cdb6b84e4576640858029d3ea6f3c99d15
miomaeshima <miomamomo@gmail.com>
2021-05-08 10:13:13 +0900

    my first commit on Progress Bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,48 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES = {
+  large:{
+  },
+  medium:{
+  },
+  small:{
+  }
+}  
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  if (size==="large"){
+    let Component = LargeProgressBar;
+    let Bar = LargeBar;
+  }
+
+  return <Component role="progressbar" aria-valuenow={value} aria-valuemin="0" aria-valuemax="100">
+  <Bar value={value}></Bar>
+  </Component>
 };
+
+const Component = styled.div`
+position: relative;
+height: 24px;
+width: 370px;
+border-radius: 8px;
+background-color: ${COLORS.transparentGray15};
+box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+`;
+
+const LargeProgressBar = styled(Component)`
+`;
+
+const Bar = styled.div`
+position: absolute;
+top:4px;
+left:4px;
+height: 16px;
+width: ${(props=>{return (props.value/100*362 + "px")})};
+background-color: ${COLORS.primary};
+border-radius: ${props=>(props.value===100) ? "4px" : "4px 0px 0px 4px"};
+`;
+
+const LargeBar = styled(Bar)`
+`;
 
 export default ProgressBar;
